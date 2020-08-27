520ecb01a5c9d249cdee26fe4ab2692f35489afc
Jeyko Caicedo <jakeactually@outlook.com>
2020-08-27 11:13:54 -0500

    day 19

--- 19/b.fsx
+++ 19/b.fsx
@@ -1,5 +1,6 @@
 open System
 open System.IO
+open System.Text.RegularExpressions
 
 let lines = [ for line in File.ReadAllLines("input.txt") do line ]
 
@@ -12,42 +13,45 @@ let mutable pairs = List.sortBy (String.length << snd) [
 
 let mutable mol = List.head <| List.skip 44 lines
 
-type Path = Path of string * int
+type Path = Path of string * int * int
 
-let value (Path(s, i)) = s
+let value (Path(s, _, _)) = s
 
-let rec intersperse xs a b n =
-    match xs with
-    | [y] -> [y]
-    | y :: ys ->
-        if n = 0 then
-            y :: b :: intersperse ys a b (n - 1)
-        else
-            y :: a :: intersperse ys a b (n - 1)
-    | [] -> []
+let depth (Path(_, d, _)) = d
 
-let outs (s: string) (k: string) (v: string): string list =
-    let parts = [ for x in s.Split([|k|], StringSplitOptions.None) do x ]
+let power (Path(_, _, p)) = p
 
-    [ for i in 0..parts.Length - 2 do
-        String.Concat<string> <| intersperse parts k v i ]
+let outs (str: string) (k: string) (v: string) =
+    let regexObj = Regex(k)
+    let mutable m = regexObj.Match(str)
 
-let out (Path(s, i)) =
-    let ops = List.filter (fun (k: string, v: string) -> mol.Contains(v)) pairs
-    List.concat [ for (k, v) in ops do List.map (fun s2 -> Path(s2, i + 1)) (outs s v k) ]
+    [
+        while m.Success do
+            let result = str.Substring(0, m.Index) + v + str.Substring(m.Index + m.Length)
+            m <- regexObj.Match(str, m.Index + 1)
+            result
+    ]
 
-let mutable min: Path = Path(mol, 0)
+let out (Path(s, i, _)) =
+    let ops = List.filter (fun (_, v: string) -> s.Contains(v)) pairs
+    List.concat [ for (k, v) in ops do List.map (fun s2 -> Path(s2, i + 1, 100 - ops.Length)) (outs s v k) ]
+
+let mutable current: Path = Path(mol, 0, 100)
 let mutable all: Path list = []
 let mutable set: Set<string> = Set.empty
 
-while value min <> "e" do
-    printfn "%O" min
+while value current <> "e" do
+    printfn "%A" current
 
-    let ws = List.sortBy (fun (Path(s, i)) -> s.Length) <| (List.filter (fun (Path(s, i)) -> not <| set.Contains(s)) <| List.concat [all; out min])
-    
-    set <- Set.union set (Set.ofList <| List.map value ws)
+    let result = List.filter (not << set.Contains << value) <| out current
+
+    set <- set.Add <| value current
+
+    let ws = List.sortBy power <| List.concat [all; result]
 
     match ws with
     | x :: xs ->
-        min <- x
+        current <- x
         all <- xs
+
+printfn "%A" current
