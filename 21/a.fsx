bc6befe23bd56162f18555c783d28940d348a352
Jeyko <jakeactually@outlook.com>
2020-08-28 20:46:07 +0200

    21

---
+++ 21/a.fsx
@@ -0,0 +1,13 @@
+(*
+    Hit Points: 104
+    Damage: 8
+    Armor: 1
+
+    Enemy stats sum 9
+    So my stats must sum at least 9
+
+    I strike first
+    So I'll win if true damage is greater that the life difference (4)
+    
+    The minimun winning combination cost 78
+*)
---
+++ 21/b.fsx
@@ -0,0 +1,13 @@
+(*
+    Hit Points: 104
+    Damage: 8
+    Armor: 1
+
+    Enemy stats sum 9
+    So my stats must sum at most 9
+
+    I strike first
+    So I'll lose if true damage is less that the life difference (4)
+    
+    The maximun losing combination cost 148
+*)
