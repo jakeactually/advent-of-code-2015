82ed9bab92b9819c0944932b6d38e5c1f5073184
Jeyko Caicedo <jakeactually@outlook.com>
2020-08-28 12:32:56 -0500

    day 20

--- 20/b.fsx
+++ 20/b.fsx
@@ -1,10 +1,12 @@
+open System
+
 let target = 29000000
 let size = target / 10
 
 let arr = Array.create size 0
 
 for i in 1..size do
-    for j in i..i..size - 1 do
-        arr.[j] <- arr.[j] + i * 10
+    for j in i..i..Math.Min(i + i * 50, size - 1) do
+        arr.[j] <- arr.[j] + i * 11
 
 Array.findIndex (fun x -> x >= target) arr |> printfn "%A"
