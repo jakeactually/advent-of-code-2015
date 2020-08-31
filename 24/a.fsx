0e7d9d56e26052fa41e1be766ed4eb8d3e2e67fa
Jeyko Caicedo <jakeactually@outlook.com>
2020-08-31 12:51:41 -0500

    day 24

---
+++ 24/a.fsx
@@ -0,0 +1,20 @@
+open System.IO
+
+let nums = File.ReadAllLines("input.txt") |> Array.map int |> Array.toList
+
+let mutable groups: int list list = []
+
+let rec out (n: int list) (ns: int list) =
+    if List.sum n = 516 then
+        groups <- n :: groups
+
+    for (n2, i) in List.zip ns [1..ns.Length] do
+        out (n2 :: n) (List.skip i ns)
+
+out [] nums
+
+groups
+    |> List.filter (fun l -> l.Length = 6)
+    |> List.map (List.fold (*) 1L << List.map int64)
+    |> List.min
+    |> printf "%A"
